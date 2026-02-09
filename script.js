b45e8ab1ba6b3f221ea210cceb499169e53fa08d
langgengabadi <51090281+langgengabadi@users.noreply.github.com>
2026-02-09 17:00:27 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,34 +1,33 @@
-const folderId = "13XwzkYwGyHy-e4XJHQqruEYQNbeVMkAz"; // Ganti dengan folder ID Drive client
-const apiKey = "AIzaSyBoBNByeXsZIgIQDNTsEBpVhjEKlUHGhns"; // Ganti API Key Google Drive
-const clientName = "NAMA_CLIENT"; // Bisa dinamis dari URL atau input
+const folderId = "13XwzkYwGyHy-e4XJHQqruEYQNbeVMkAz"; // ganti Folder ID kamu
+const apiKey = "AIzaSyBoBNByeXsZIgIQDNTsEBpVhjEKlUHGhns";
+const clientName = "NAMA_CLIENT";
 
 const gallery = document.getElementById("gallery");
-const selectedP = document.getElementById("selected");
 let selectedPhotos = [];
 
-// Ambil file dari Drive
 fetch(`https://www.googleapis.com/drive/v3/files?q='${folderId}'+in+parents&fields=files(id,name)&key=${apiKey}`)
-.then(res=>res.json())
-.then(data=>{
-  data.files.forEach(file=>{
+.then(res => res.json())
+.then(data => {
+  data.files.forEach(file => {
     const img = document.createElement("img");
-    img.src = `https://drive.google.com/uc?id=${file.id}`;
+    img.src = `https://drive.google.com/uc?id=${file.id}`; // full-res image
     img.alt = file.name;
 
-    img.addEventListener("click",()=>{
-      if(selectedPhotos.includes(file.id)){
-        selectedPhotos = selectedPhotos.filter(id=>id!==file.id);
+    img.addEventListener("click", () => {
+      if (selectedPhotos.includes(file.id)) {
+        selectedPhotos = selectedPhotos.filter(id => id !== file.id);
         img.style.border = "";
-      }else{
+      } else {
         selectedPhotos.push(file.id);
         img.style.border = "3px solid green";
       }
-      selectedP.innerText = `${selectedPhotos.length} Foto Terpilih`;
+
+      document.getElementById("selected").innerText = `${selectedPhotos.length} Foto Terpilih`;
 
       // Kirim pilihan ke Firebase
-      fetch(`https://photoselectorapp-default-rtdb.firebaseio.com/clients/${clientName}.json`,{
-        method:"PUT",
-        body: JSON.stringify({selectedPhotos})
+      fetch(`https://YOUR_FIREBASE_URL/clients/${clientName}.json`, {
+        method: "PUT",
+        body: JSON.stringify({ selectedPhotos })
       });
     });
 
